a172da7dc56674279df58c2f3b4f101bade7afae
Lucas TS <seu@email.com>
2025-05-29 21:39:32 +0000

    configure git on dev container

---
+++ .devcontainer/setup-get.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git config --global user.name "Lucas TS"
+git config --global user.email "lucas@lucas-ts.com"
+git config --global color.ui auto
+
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.last "log -1 HEAD"
